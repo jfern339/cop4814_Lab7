4eaa8ebc3fc6907b633e2d77da2113a31e3a9c76
jfern339 <37593055+jfern339@users.noreply.github.com>
2018-03-20 17:59:43 -0400

    Add files via upload

---
+++ GameFactory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+
+
+namespace GamesScoresLibrary
+{
+    public class GameFactory
+    {
+        StreamWriter sw;
+        XmlSerializer serial;
+        List<Games> gamesList;
+        const string FilePath = @"..\..\games.xml";
+
+        public void CreateGameList()
+        {
+            gamesList = new List<Games>();
+            Games G = new Games("Scarlet Knights", 78, "Buckeyes", 74);
+            gamesList.Add(G);
+            G = new Games("Patriots", 0, "Eagles", 34);
+            gamesList.Add(G);
+            G = new Games("Yankees", 8, "Red Sox", 1);
+            gamesList.Add(G);
+            G = new Games("Hurricanes", 28, "Golden Panthers", 30);
+            gamesList.Add(G);
+            G = new Games("Gators", 3, "Seminoles", 45);
+            gamesList.Add(G);
+            G = new Games("Marlins", 2, "Mets", 3);
+            gamesList.Add(G);
+                       
+        }
+
+        public Boolean SerializeGameList()
+        {
+            if (gamesList != null)
+            {
+                serial = new XmlSerializer(gamesList.GetType());
+                sw = new StreamWriter(FilePath);
+                serial.Serialize(sw, gamesList);
+                sw.Close();
+                return true;
+            }
+            else
+                return false;
+            
+        }
+
+        public List<Games> DeserializeGamesList(out string resultMessage)
+        {
+            try
+            {
+                gamesList = new List<Games>();
+                StreamReader sr = new StreamReader(FilePath);
+                serial = new XmlSerializer(gamesList.GetType());
+                gamesList = (List<Games>)serial.Deserialize(sr);
+                sr.Close();
+                resultMessage = "Success";
+                return gamesList;
+            }
+            catch (Exception ex)
+            {
+                resultMessage = "Exception thrown: " + ex.Message;
+                return null;
+            }
+        }
+    }
+}
---
+++ Games.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesScoresLibrary
+{
+    public class Games
+    {
+        public string Team1 { get; set; }
+        public int Team1score { get; set; }
+        public string Team2 { get; set; }
+        public int Team2score { get; set; }
+
+        public Games() { }
+
+        public Games(string Team1, int Team1score, string Team2, int Team2score)
+        {
+            this.Team1 = Team1;
+            this.Team1score = Team1score;
+            this.Team2 = Team2;
+            this.Team2score = Team2score;
+        }
+
+        public override string ToString()
+        {
+            string score1 = "(" + Team1score + ")";
+            string hyphen = " - ";
+            string score2 = "(" + Team2score + ")";
+
+            return string.Format("{0} {1} {2} {3} {4}", Team1, score1, hyphen, Team2, score2);
+        }
+
+    }
+}
+
